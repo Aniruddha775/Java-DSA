ac7e38e108998fcd2b6049809d8ab719e7dd4022
Aniruddha775 <aniruddhadc.dev@gmail.com>
2025-03-26 20:18:48 +0530

    MinInRotatedArray

---
+++ src/MinInRotatedArray.java
@@ -0,0 +1,32 @@
+public class MinInRotatedArray {
+    static int findMin(int[] arr){
+        int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                // keep the minimum:
+                ans = Math.min(ans, arr[low]);
+
+                // Eliminate left half:
+                low = mid + 1;
+
+            } else { //if right part is sorted:
+
+                // keep the minimum:
+                ans = Math.min(ans, arr[mid]);
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return ans;
+
+    }
+    public static void main(String[] args){
+        int[] arr={3,1,2};
+        System.out.println(findMin(arr));
+    }
+}
