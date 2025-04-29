e2be66b1d1017ce28b028584ffeb514a94fff88c
Aniruddha775 <aniruddhadc.dev@gmail.com>
2025-04-29 20:32:30 +0530

    FindSecondLargestElement

---
+++ src/com/aniruddha/Arrays_and_Strings/FindSecondLargestElement.java
@@ -0,0 +1,22 @@
+package com.aniruddha.Arrays_and_Strings;
+
+public class FindSecondLargestElement {
+    static int secondLargest(int[] arr){
+        int largest=arr[0];
+        int secondlargest=Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>largest){
+                secondlargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>secondlargest){
+                secondlargest=arr[i];
+            }
+        }
+        return secondlargest;
+    }
+    public static void main(String[] args) {
+        int[] arr={-1,2,-5,7,3,-8,9};
+        System.out.println(secondLargest(arr));
+    }
+}
